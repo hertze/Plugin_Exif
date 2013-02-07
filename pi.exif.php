56b3abf148c616752215d378211d6bb698969af5
Joakim Hertze <joakim.hertze@gmail.com>
2013-02-07 20:58:41 +0100

    Clean up of regex patterns. Addition of a few comments.

--- pi.exif.php
+++ pi.exif.php
@@ -2,10 +2,10 @@
 class Plugin_exif extends Plugin {
 
     var $meta = array(
-        'name'       => 'Exif',
+        'name'       => 'Exif extractor',
         'version'    => '0.1',
         'author'     => 'Joakim Hertze',
-        'author_url' => 'http://hertze.com'
+        'author_url' => 'http://magiso.com'
     );
 
     public function index()
@@ -14,16 +14,27 @@ class Plugin_exif extends Plugin {
       
       if ($imagepath) {
       	
-      	$imagepath = trim($imagepath, '/'); // We need to rid ourselves of any leading slashes for this to work
-      	
+      	// We need to rid ourselves of any leading slashes for this to work
+      	$imagepath = trim($imagepath, '/');
+      	      	
       	try { 
       		$raw = exif_read_data($_SERVER['DOCUMENT_ROOT'].$imagepath, 0, true); 
       		} catch (Exception $e) { 
 	      		var_dump($e->__toString()); 
 	      	} 
+	      	
+	    // The following is to prevent division by zero when no Exif data is embedded in the image.	      
 	    if (strlen($raw['EXIF']['FocalLength'])>0) {
       			$flength = round(floatval(substr($raw['EXIF']['FocalLength'], 0, strpos($raw['EXIF']['FocalLength'],'/')))/floatval(substr($raw['EXIF']['FocalLength'], strpos($raw['EXIF']['FocalLength'],'/')+1, strlen($raw['EXIF']['FocalLength'])))).'mm';
       	}
+      	
+      	// Unfortunately, inconsistencies is naming between camera makers makes it necessary to customize
+      	// the cleaning up of exif data. The following takes care of Nikon and Olympus cameras. Feel free to
+      	// add more as you find necessary. 
+      	$make_pattern = array('/\ IMAGING\ CORP\./', '/\ CORPORATION/');
+      	$make_replace = array('','');
+      	$model_pattern = array('/NIKON/');
+      	$model_replace = array('');
       		
         return array(
           'imagewidth' => $raw['IFD0']['ImageWidth'],
@@ -31,7 +42,7 @@ class Plugin_exif extends Plugin {
           'datetime' => $raw['IFD0']['DateTime'],	
           'model' => $raw['IFD0']['Model'],
           'make' => $raw['IFD0']['Make'],
-          'camera' => ucfirst(strtolower(preg_replace(array('/\ IMAGING\ CORP\./', '/\ CORPORATION/'), '', $raw['IFD0']['Make']))).''.preg_replace(array('/NIKON/'), '', $raw['IFD0']['Model']),
+          'camera' => ucfirst(strtolower(preg_replace($make_pattern, $make_replace, $raw['IFD0']['Make']))).''.preg_replace($model_pattern, $model_replace, $raw['IFD0']['Model']),
           'software' => $raw['IFD0']['Software'].'s',
           'aperture' => $raw['COMPUTED']['ApertureFNumber'],
           'exposure' => $raw['EXIF']['ExposureTime'].'s',
